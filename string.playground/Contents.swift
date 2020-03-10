970d88e0ed2f74861a9bed43701ff3134fbc8cc5
Apple <apple@Apples-MacBook-Pro.local>
2020-03-10 17:51:23 +0530

    string in swift

---
+++ string.playground/Contents.swift
@@ -0,0 +1,12 @@
+var srt1 = "ashutosh wahane is my name"
+// capitalized function
+print(srt1.count)
+print(srt1.uppercased())
+
+var str2 = "the quick brown"
+print(str2)
+str2.append(" fox")
+print(str2)
+
+var str3 = ""
+print(str3.isEmpty)
