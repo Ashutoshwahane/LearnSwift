ab4bfea6442ea62acc92918d8272fb70d3783359
Apple <apple@Apples-MacBook-Pro.local>
2020-03-10 17:33:56 +0530

    vairable declaration in swift

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Data types in Swift
+// Declaration of variable
+
+var str = "hello world"
+var intNum = 10
+var deciNum = 10.10
+var is_bool = true
+
+print(str)
+print(intNum)
+print(deciNum)
+print(is_bool)
