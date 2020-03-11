fe73889b4d1b184f6e009150583f9e5cfe8d3b08
Apple <apple@Apples-MacBook-Pro.local>
2020-03-11 23:28:18 +0530

    control flow in swift

---
+++ control_flow.playground/Contents.swift
@@ -0,0 +1,14 @@
+// control flow
+let numbers = [12,43,567,23,84,02]
+for num in numbers{
+//    print(num)
+    // if statement (parentheses are optional)
+    if num%2==0{
+        print("\(num) is even number")
+    }
+    else{
+        print("\(num) is odd number")
+    }
+}
+
+
