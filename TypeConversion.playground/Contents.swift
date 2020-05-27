85528b922c4ed6965ae5dd0ea098dfad450bc32d
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-05-28 00:59:15 +0530

    type conversion Int and Double

---
+++ TypeConversion.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Integer and Floating-Point Conversion
+let three = 3
+let pointonefouronefivenine = 0.14159
+let pi = Double(three) + pointonefouronefivenine
+// pi equals 3.14159, and is inferred to be of type Double
+// three was declare as Int but converted to Double(three)
+
+let integerPi = Int(pi)
+// from Double to Int
+
+
