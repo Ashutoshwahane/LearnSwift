98b1aa6b0e24659742f04faf236516c6bad376ca
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-05-28 00:21:14 +0530

    declare variable, constant and boolean

---
+++ LearnSwift.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// Variables
+var str = "Hello, playground"
+var a:Int = 1
+var b:Int = 1
+a = 2
+str = "Ashutosh Wahane"
+print(a+b)
+print(a-b)
+print(a*a)
+var myNumberOfApple = 10
+
+// Constants
+let no = 10
+print(no)
+
+// DataType
+var name:String
+name = "Ashutosh Wahane"
+var age:Int
+age = 22
+
+// Boolean
+var IamGod : Bool = true
+print(IamGod)
+
+
