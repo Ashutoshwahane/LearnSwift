c3f29a0ad0efcd9dfab96a747e58af7bfe58736f
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-05-28 16:42:49 +0530

    operators in swift

---
+++ Basic Operation.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Assignment Operator
+let b = 10
+var a = 5
+a = b
+print(a)
+
+let (x,y) = (10,9)
+print("x value is :",x,"\n y value is :",y)
+// x is equal to 1, and y is equal to 2
+
+if x == y {
+    print("true")
+}
+else{
+    print("false not same")
+}
+
+// Arithmatic Operator
+1+2
+2-1
+1*3
+10.5/2.5
+
+//String Concatenation
+
+"Ashutosh " + "Wahane"
+
+9%4 // remainder operator
+
+// unary operator
+let three = 3
+let minusthree = -three
+let plusthree = -minusthree
+
+// Compound Opeator
+
+var one = 1
+one += 2
+
+
+// comparison operator
+/*
+    1==1
+    2!=1
+    2>1
+    1<2
+    1>=1
+    2<=1
+ */
+
+(1,"apple")<(2,"mango")
+
+
