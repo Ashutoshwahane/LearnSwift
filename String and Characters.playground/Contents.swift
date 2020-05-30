8a89572ef96e8d20de4354de048a1fa5b6606890
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-05-30 09:13:48 +0530

    String Interpolation and Concatenation

--- String and Characters.playground/Contents.swift
+++ String and Characters.playground/Contents.swift
@@ -57,6 +57,63 @@ for i in "Ashutosh"{
 
 let exclamationMark : Character = "!"
 
+let catCharacter : [Character] = ["C","A","T"]
+
+let catString  = String(catCharacter)
+print(catString)
+
+// Concatenation
+let str1 = "hello"
+let str2 = "there"
+var welcome = str1 + str2
+
+// Append String
+var instruction = "look over"
+instruction += str2
+
+welcome.append(exclamationMark)
+
+// If you’re using multiline string literals to build up the lines of a longer string, you want every line in the string to end with a line break, including the last line. For example:
+let badStart = """
+one
+two
+"""
+let end = """
+three
+"""
+print(badStart + end)
+
+let goodStart = """
+one
+two
+
+"""
+print(goodStart + end)
+
+// String Interpolation
+
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+
+print("now I understand this \(multiplier) if you want to print the value use backslash and parentheses")
+print(#"if you dont want to print the value \(multiplier) this is going to ignor it because of hash"#)
+
+print(#"6 times 7 into 3 \#(6*7*multiplier)"#)
+
+// Counting Character
+let unusualString = "Ashutosh 👨🏽‍💻,wahane🙅🏻‍♂️,enjoying ✪"
+//let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+//print("unusualMenagerie has \(unusualMenagerie.count) characters")
+
+print("unusualString has \(unusualString.count) Charactor")
+
+let greeting = "Ashutosh Wahane"
+greeting[greeting.startIndex]
+greeting[greeting.index(before: greeting.endIndex)]
+greeting[greeting.index(after: greeting.startIndex)]
+
+let index = greeting.index(greeting.startIndex,offsetBy: 5)
+greeting[index]
 
 
 
