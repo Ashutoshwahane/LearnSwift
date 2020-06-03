9b166198e21001fae80ee166a5a3318cc929cbe8
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-06-03 11:22:42 +0530

    for in loop

--- control_flow.playground/Contents.swift
+++ control_flow.playground/Contents.swift
@@ -34,6 +34,57 @@ else {
     }
 }
 
+// For In Loop
+
+let name = ["ashutosh","goldy","wills"]
+for i in name{
+    print(i)
+}
+
+// iterate over the dictionary
+let numberOfLegs = ["spider":8,"ant":6,"cat":4]
+for (animalName,legCount) in numberOfLegs{
+    print("\(animalName) has \(legCount) legs")
+}
+
+// for in loop with numeric range
+for i in 1...5{
+    print("number is \(i)")
+}
+
+// if we dont need value everytime
+
+let base = 3
+let power = 10
+var ans = 1
+for _ in 1...power {
+    ans *= base
+    print(ans)
+}
+
+let minute = 60
+for tickmark in 1...minute{
+    print(tickmark)
+}
+let minuteInterval = 5
+for tickmark in stride(from: 0, to: minute, by: minuteInterval){
+    print(tickmark)
+}
+
+let hour = 12
+let hourInterval = 3
+for tickmark in stride(from: 3, through: hour, by: hourInterval){
+    print(tickmark)
+}
+
+// while loop
+
+
+
+
+
+
+
 
 
 
